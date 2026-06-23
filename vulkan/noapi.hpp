8a6505aa2aadb53d2201f73b6ee5380b51b443af
Joshua Dahl <joshuadahl@unr.edu>
2026-06-23 14:07:33 -0700

    [Vulkan] Compute Pipeline

--- vulkan/noapi.hpp
+++ vulkan/noapi.hpp
@@ -9,6 +9,15 @@
 #include <functional>
 #include <expected>
 
+
+#ifndef NOAPI_NO_EXCEPTIONS
+	struct VkResultExecption : std::exception {
+		VkResult result;
+
+		VkResultExecption(VkResult result) : result(result) {}
+	};
+#endif
+
 struct GpuVulkanDefault {
 	VkInstance instance = VK_NULL_HANDLE;
 	VkDebugUtilsMessengerEXT messenger = VK_NULL_HANDLE;
@@ -22,9 +31,21 @@ std::expected<GpuVulkanDefault, std::string> gpuSetupDefaultVulkan(
 	std::function_ref<VkSurfaceKHR(VkInstance)> surface_loader, std::span<const char*> extra_extensions = {}, std::span<const char*> extra_layers = {}, bool debug = true
 );
 
+inline VkPhysicalDeviceFeatures gpuEnableRequiredVulkanFeatures(VkPhysicalDeviceFeatures features) {
+	features.shaderInt64 = true;
+	return features;
+}
+
 inline VkPhysicalDeviceVulkan12Features gpuEnableRequiredVulkan12Features(VkPhysicalDeviceVulkan12Features features) {
 	features.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES;
 	features.bufferDeviceAddress = true;
+	features.timelineSemaphore = true;
+	return features;
+}
+
+inline VkPhysicalDeviceVulkan13Features gpuEnableRequiredVulkan13Features(VkPhysicalDeviceVulkan13Features features) {
+	features.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES;
+	features.synchronization2 = true;
 	return features;
 }
 
@@ -35,15 +56,23 @@ struct GpuQueue {
 	uint32_t queue_family;
 	bool is_graphics_queue;
 
+	VkAllocationCallbacks* callbacks = nullptr;
+
 	// Memory Allocator
-	VmaAllocator allocator;
+	VmaAllocator allocator = VK_NULL_HANDLE;
 	// Mapping from gpu* (Device Addresses) to buffer allocations
 	std::unordered_map<VkDeviceAddress, std::pair<VkBuffer, VmaAllocation>> allocations;
 	// Mappings between cpu and gpu pointers
 	std::unordered_map<void*, VkDeviceAddress> host2gpu;
 	std::unordered_map<VkDeviceAddress, void*> gpu2host;
+	VkPipelineLayout pipeline_layout = VK_NULL_HANDLE; // TODO: Needs to be freed! // TODO: Do we need seperate ones for compute and graphics?
 };
-std::optional<GpuQueue> gpuCreateQueue(VkInstance instance, VkPhysicalDevice gpu, VkDevice device, VkQueue queue = VK_NULL_HANDLE, uint32_t queue_family = -1, bool is_graphics_queue = true);
-inline std::optional<GpuQueue> gpuCreateQueue(const GpuVulkanDefault& vulkan) {
-	return gpuCreateQueue(vulkan.instance, vulkan.gpu, vulkan.device, vulkan.graphics_queue, vulkan.graphics_queue_family, true);
+std::optional<GpuQueue> gpuCreateQueue(VkInstance instance, VkPhysicalDevice gpu, VkDevice device, VkQueue queue = VK_NULL_HANDLE, uint32_t queue_family = -1, VkAllocationCallbacks* callbacks = nullptr, bool is_graphics_queue = true);
+inline std::optional<GpuQueue> gpuCreateQueue(const GpuVulkanDefault& vulkan, VkAllocationCallbacks* callbacks = nullptr) {
+	return gpuCreateQueue(vulkan.instance, vulkan.gpu, vulkan.device, vulkan.graphics_queue, vulkan.graphics_queue_family, callbacks, true);
 }
+
+struct GpuPipeline {
+	// VkShaderModule compute_module; // TODO: Do we have a need to carry this around?
+	VkPipeline pipeline;
+};
