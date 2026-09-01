9279d2f9560da44744e63667543895e426476956
Joshua Dahl <joshuadahl@unr.edu>
2026-09-01 13:21:37 -0700

    [WebGPU] Texture Allocation
    Allocates textures (using approximately the same algorithm as buffer allocation) from a matching monolithic texture.
    We have certain properties hashed and textures are rounded up to the closest matching texture.

--- webgpu/noapi.hpp
+++ webgpu/noapi.hpp
@@ -7,11 +7,15 @@
 
 #include <expected>
 #include <functional>
+#include <optional>
 #include <string>
 #include <tuple>
 #include <unordered_map>
-#include <webgpu/webgpu.h>
+// #include <map>
 #include <array>
+#include <vector>
+
+#include <webgpu/webgpu.h>
 
 namespace GPU {
 #ifdef __cpp_lib_function_ref
@@ -68,14 +72,94 @@ struct GpuQueue {
 
 	struct MonobufferRange {
 		uint8_t buffer;
-		size_t start, end;
+		uint32_t start, end;
 		size_t size() { return end - start; }
 	};
 	std::unordered_map<gpu*, std::tuple<MonobufferRange, void*, MEMORY>> allocations;
 	std::unordered_map<void*, gpu*> cpu2gpu;
-	std::vector<MonobufferRange> freelist;
+	std::unordered_map<gpu*, GpuTexture*> gpu2textures;
+	std::vector<MonobufferRange> buffer_freelist;
 
 	std::vector<std::pair<std::function<void()>, size_t>> code_pending_submission_finished;
+
+	struct TextureHash {
+		TEXTURE type = TEXTURE_2D; ///< Dimensionality and view type.
+		uvec3 dimensions = {1, 1, 1}; ///< Dimensions in texels.
+		uint32_t sampleCount = 1; ///< MSAA sample count.
+		FORMAT format = FORMAT_NONE; ///< Texel format.
+
+		static TextureHash from_descriptor(const GpuTextureDesc& desc) {
+			auto out = GpuQueue::TextureHash{desc.type, desc.dimensions, desc.sampleCount, desc.format};
+			out.dimensions.x = std::bit_ceil(desc.dimensions.x);
+			out.dimensions.y = std::bit_ceil(desc.dimensions.y);
+			out.dimensions.z = std::bit_ceil(desc.dimensions.z);
+			return out;
+		}
+
+		bool operator==(const TextureHash& o) const {
+			return type == o.type && dimensions.x == o.dimensions.x && dimensions.y == o.dimensions.y && dimensions.z == o.dimensions.z && sampleCount == o.sampleCount && format == o.format;
+		}
+
+		struct Hasher {
+			uint64_t operator()(const TextureHash& t) const {
+				return std::hash<size_t>{}(t.type) ^ std::hash<size_t>{}(t.dimensions.x) ^ std::hash<size_t>{}(t.dimensions.y) ^ std::hash<size_t>{}(t.dimensions.z)
+					^ std::hash<size_t>{}(t.sampleCount) ^ std::hash<size_t>{}(t.format);
+			}
+		};
+		// struct Comparator {
+		// 	bool operator()(const TextureHash& a, const TextureHash& b) const {
+		// 		if (a.type != b.type)
+		// 			return a.type < b.type;
+
+		// 		if (a.dimensions.x != b.dimensions.x)
+		// 			return a.dimensions.x < b.dimensions.x;
+
+		// 		if (a.dimensions.y != b.dimensions.y)
+		// 			return a.dimensions.y < b.dimensions.y;
+
+		// 		if (a.dimensions.z != b.dimensions.z)
+		// 			return a.dimensions.z < b.dimensions.z;
+
+		// 		if (a.sampleCount != b.sampleCount)
+		// 			return a.sampleCount < b.sampleCount;
+
+		// 		return a.format < b.format;
+		// 	}
+		// };
+	};
+	std::vector<std::tuple<uint32_t, WGPUTexture, WGPUTextureView>> storage_monotextures;
+	std::unordered_map<TextureHash, size_t, TextureHash::Hasher> storage_monotextures_lookup;
+	std::vector<std::tuple<uint32_t, WGPUTexture, WGPUTextureView>> sampled_monotextures;
+	std::unordered_map<TextureHash, size_t, TextureHash::Hasher> sampled_monotextures_lookup;
+
+	struct MonotextureRange {
+		const static MonotextureRange INVALID; // = {-1, -1, -1}
+		static constexpr uint32_t STORAGE_BIT = 0x80000000u;
+		static constexpr uint32_t INDEX_MASK  = 0x7FFFFFFFu;
+
+		uint32_t _index; // top bit stores storage (versus sampled) flag
+		uint32_t start, end;
+
+		bool storage() const {
+			return (_index & STORAGE_BIT) != 0;
+		}
+
+		void set_storage(bool storage) {
+			if (storage)
+				_index |= STORAGE_BIT;
+			else
+				_index &= INDEX_MASK;
+		}
+
+		uint32_t index() const {
+			return _index & INDEX_MASK;
+		}
+
+		void set_index(uint32_t value) {
+			_index = (_index & STORAGE_BIT) | (value & INDEX_MASK);
+		}
+	};
+	std::vector<MonotextureRange> texture_freelist;
 };
 
 GpuQueue* gpuCreateQueue(WGPUAdapter adapter, WGPUDevice device, WGPULimits limits, CpuAllocatorFunc allocator = default_::cpu_allocator);
@@ -90,3 +174,18 @@ struct GpuCommandBuffer {
 
 	std::vector<std::function<void()>> code_pending_submission_finished;
 };
+
+struct GpuTexture {
+	GpuTextureDesc descriptor;
+	std::optional<GpuQueue::MonotextureRange> range = {};
+	WGPUTexture texture;
+};
+
+struct GpuTextureDescriptorImpl {
+	TEXTURE type = TEXTURE_2D; ///< Dimensionality and view type.
+	uint32_t width = 1, height = 1; ///< Dimensions in texels.
+	uint32_t baseMip = 0;
+	uint32_t mipCount = 1;
+	GpuQueue::MonotextureRange range;
+};
+static_assert(sizeof(GpuTextureDescriptorImpl) == sizeof(GpuTextureDescriptor), "GPU Texture Descriptors of The Wrong Size");
